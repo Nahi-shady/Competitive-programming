45d1509fa85bcef0867b39d499b5535e89df4e58
Nahi-shady <nahiyo86@gmail.com>
2023-12-12 04:30:28 +0800

    Added solution for Element Appearing More Than 25% In Sorted Array

---
+++ Python-Track/element-appearing-more-than-25-in-sorted-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        if len(arr) <= 2:
+            return arr[0]
+
+        count = 0
+        quarter = 0.25*len(arr)
+        same_number = arr[0]
+
+        for i in range(len(arr)):
+            if arr[i] == same_number:
+                count += 1
+                if count > quarter:
+                    return arr[i]
+            else:
+                count = 1
+                same_number = arr[i]
+        
+        return arr[i]
