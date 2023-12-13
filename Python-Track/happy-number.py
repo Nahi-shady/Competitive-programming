6163d2f563844e9ae76a3f66a8d228cdb96731ed
Nahi-shady <nahiyo86@gmail.com>
2023-12-13 18:36:12 +0800

    Added solution for Happy Number

--- Python-Track/happy-number.py
+++ Python-Track/happy-number.py
@@ -1,10 +1,14 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
+        visited = set()
         summation = n
-        impossible = (2, 4, 16, 37, 58, 89, 145, 42)
+        impossible = (2, 4, 16, 37, 58, 89, 145, 42, 20)
+
         while summation != 1:
-            if summation in impossible:
+            if summation in visited:
                 return False
-            summation = sum([int(i)*int(i) for i in str(summation)])
+                
+            visited.add(summation)
+            summation = sum(int(i)**2 for i in str(summation))
         
         return True
