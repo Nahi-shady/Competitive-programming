8d71f117397aeb3d4478f1d5c2c20c6c9ee5b6a2
Nahi-shady <nahiyo86@gmail.com>
2023-12-26 23:31:58 +0800

    Added solution for Sort the People

---
+++ progress-sheet/sort-the-people.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        for i in range(len(names)):
+            min_idx = i
+            for j in range(i+1, len(names)):
+                if heights[j] > heights[min_idx]:
+                    min_idx = j
+
+            heights[i], heights[min_idx] = heights[min_idx], heights[i]
+            names[i], names[min_idx] = names[min_idx], names[i]
+
+        return names
