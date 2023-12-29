ad4bd8f6149b794b2f7b9ff5aefdf4753ce33bac
Nahi-shady <nahiyo86@gmail.com>
2023-12-30 00:10:59 +0800

    Added solution for Largest Number

--- progress-sheet/largest-number.py
+++ progress-sheet/largest-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         nums = list(map(str, nums))
-        print(nums)
         nums.sort(key=lambda arr: arr*10, reverse=True)
+        
         if nums[0] == '0':
             return '0'
-        print(nums)
+
         return ''.join(nums)
