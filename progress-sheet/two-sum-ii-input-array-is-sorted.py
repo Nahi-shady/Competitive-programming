93736334d039e9ead5e326a4dbc51395481b839d
Nahi-shady <nahiyo86@gmail.com>
2024-01-02 23:31:46 +0800

    Added solution for Two Sum II - Input Array Is Sorted

--- progress-sheet/two-sum-ii-input-array-is-sorted.py
+++ progress-sheet/two-sum-ii-input-array-is-sorted.py
@@ -1,18 +1,11 @@
-class Solution(object):
-    def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        l = 0
-        r = len(numbers) - 1
-        while l < r:
-            if numbers[l] + numbers[r] == target:
-                return [l+1, r+1]
-            elif numbers[l] + numbers[r] > target:
-                r -= 1
-            else:
-                l += 1
-                
-        
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        right = len(numbers)-1
+        left = 0
+        while left < right:
+            if numbers[right] + numbers[left] == target:
+                return [left+1, right+1]
+            if numbers[right] + numbers[left] > target:
+                right -= 1
+            elif numbers[right] + numbers[left] < target:
+                left += 1
