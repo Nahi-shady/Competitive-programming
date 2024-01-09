db67ba6e7b37c61b1f7ea67b9f9b08a38720a1a1
Nahi-shady <nahiyo86@gmail.com>
2024-01-09 17:02:33 +0800

    Added solution for Remove Element

--- progress-sheet/remove-element.py
+++ progress-sheet/remove-element.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
         i = 0
         for x in nums:
             if x != val:
