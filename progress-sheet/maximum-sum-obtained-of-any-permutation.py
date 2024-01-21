5fc261fdd1a9505cb7127435b709a778d66c7ce1
Nahi-shady <nahiyo86@gmail.com>
2024-01-21 11:17:22 +0100

    Added solution for Maximum Sum Obtained of Any Permutation

---
+++ progress-sheet/maximum-sum-obtained-of-any-permutation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
+        n = max(i[1] for i in requests)
+        prefix = [0]*(n+2)
+
+        for start, end in requests:
+            prefix[start] += 1
+            prefix[end+1] -= 1 
+        prefix = list(accumulate(prefix))[:-1]
+        nums.sort(reverse=True)
+        prefix.sort(reverse=True)
+        ans = 0
+        for i in range(n+1):
+            ans += prefix[i]*nums[i]
+
+        return ans % (10**9 + 7)
