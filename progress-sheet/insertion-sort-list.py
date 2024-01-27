3eee123706bd5c6aa22ddbce2af265167b4316af
Nahi-shady <nahiyo86@gmail.com>
2024-01-27 23:03:37 +0100

    Added solution for Insertion Sort List

---
+++ progress-sheet/insertion-sort-list.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head.next:
+            return head
+        dummy = ListNode(-float('inf'), head)
+        curr = dummy.next
+        while curr and curr.next:
+            next = curr.next
+            if curr.val > next.val:
+                left = dummy
+                while left.next and left.next.val < next.val:
+                    left = left.next
+                right = left.next
+                curr.next = next.next
+                next.next = right
+                left.next = next
+                curr = right
+            else:
+                
+                curr = curr.next
+
+        return dummy.next
+            
