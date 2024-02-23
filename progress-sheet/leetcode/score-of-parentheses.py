ea13899ffdcdd71b07c6cebdce97e3bddf0c225e
Nahi-shady <nahiyo86@gmail.com>
2024-02-23 16:19:27 +0100

    Add solution for Score of Parentheses

--- progress-sheet/leetcode/score-of-parentheses.py
+++ progress-sheet/leetcode/score-of-parentheses.py
@@ -1,13 +1,13 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        stack = []
+        stack = 0
         score = 0
         for i in range(len(s)):
             if s[i] == ')':
                 if s[i - 1] == '(':
-                    score += 2**(len(stack)-1)
-                stack.pop()
+                    score += 2**(stack-1)
+                stack -= 1
             else:
-                stack.append(i)
+                stack += 1
             
         return score
